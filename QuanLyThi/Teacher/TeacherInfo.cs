923e3790c478b60ebc403aaeba188ff01caff81d
Mai Huu Tho <huutho27102005@gmail.com>
2024-12-09 13:50:32 +0700

    Thong tin ca nhan

--- QuanLyThi/Teacher/TeacherInfo.cs
+++ QuanLyThi/Teacher/TeacherInfo.cs
@@ -12,9 +12,47 @@ namespace QuanLyThi
 {
     public partial class TeacherInfo : Form
     {
+        private int teacherId = 1; // default
+        private string tenDangNhap = "";
         public TeacherInfo()
         {
             InitializeComponent();
+            loadThongTin();
+        }
+
+        void loadThongTin()
+        {
+            SqlRunner sqlRunner = new SqlRunner();
+            DataTable dt = sqlRunner.excuteQuery(string.Format("SELECT gv.*, tk.tenDangNhap FROM dbo.GiaoVien AS gv INNER JOIN dbo.taikhoan AS tk ON gv.maGiaoVien = tk.maNguoiDung WHERE tk.loaiNguoiDung = 'GiaoVien' AND maGiaoVien = {0}", teacherId));
+            tenDangNhap = textBoxLoginName.Text = dt.Rows[0]["tenDangNhap"].ToString();
+            textBoxName.Text = dt.Rows[0]["hoTen"].ToString();
+            textBoxEmail.Text = dt.Rows[0]["email"].ToString();
+            textBoxID.Text = dt.Rows[0]["maGiaoVien"].ToString();
+            textBoxPhone.Text = dt.Rows[0]["soDienThoai"].ToString();
+        }
+
+        private void ClickToUpdate(object sender, EventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(textBoxCurPassWord.Text) || string.IsNullOrWhiteSpace(textBoxNewPassWord.Text) || string.IsNullOrWhiteSpace(textBoxReNewPassWord.Text))
+            {
+                MessageBox.Show("Chưa điền đẩy đủ thông tin", "Notice");
+                return;
+            }
+            SqlRunner sqlRunner = new SqlRunner();
+            bool checkOldPass = (int)sqlRunner.executeScalar(string.Format("SELECT COUNT(*) FROM dbo.taikhoan WHERE loaiNguoiDung = 'GiaoVien' AND maNguoiDung = {0} AND matKhau = '{1}'", teacherId, textBoxCurPassWord.Text)) > 0 ? true : false;
+            if (!checkOldPass)
+            {
+                MessageBox.Show("Mật khẩu cũ sai", "Notice");
+                return;
+            }
+            if (!textBoxNewPassWord.Text.Equals(textBoxReNewPassWord.Text))
+            {
+                MessageBox.Show("Nhập lại mật khẩu sai", "Notice");
+                return;
+            }
+            sqlRunner.excuteNonQuery(string.Format("UPDATE dbo.taikhoan SET matKhau = '{0}' WHERE loaiNguoiDung = 'GiaoVien'  AND tenDangNhap = '{1}'", textBoxNewPassWord.Text, tenDangNhap));
+            MessageBox.Show("Cập nhật lại mật khẩu thành công");
+            textBoxCurPassWord.Text = textBoxNewPassWord.Text = textBoxReNewPassWord.Text = "";
         }
     }
 }
